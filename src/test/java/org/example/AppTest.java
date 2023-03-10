28e217b7fd32c5908a5d000953837bae2c17023f
Yashoba Wickramage <yashobaw@gmail.com>
2023-03-10 14:36:25 +0530

    test push

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,17 +1,11 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+import static org.junit.Assert.assertTrue;
+
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
